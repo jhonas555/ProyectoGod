e30ea3600d0fd92c381daeee42c1fd16ac79688c
Jhonas <Jhonas@DESKTOP-TDAE268.Home>
2023-11-23 15:55:29 -0400

    another act.

--- ClinicaJDC/src/logico/Clinica.java
+++ ClinicaJDC/src/logico/Clinica.java
@@ -4,29 +4,31 @@ import java.util.ArrayList;
 
 public class Clinica {
 	
-	public Clinica(ArrayList<Vacuna> lasVacinas, ArrayList<Vivienda> lasViviendas, ArrayList<Cita> lasCitas,
-			ArrayList<Enfermedad> lasEnfermedades) {
+	public Clinica(ArrayList<Vacuna> lasVacunas, ArrayList<Vivienda> lasViviendas, ArrayList<Cita> lasCitas,
+			ArrayList<Enfermedad> lasEnfermedades,  ArrayList<Persona> lasPersonas) {
 		super();
-		this.lasVacinas = lasVacinas;
+		this.lasVacunas = lasVacunas;
 		this.lasViviendas = lasViviendas;
 		this.lasCitas = lasCitas;
 		this.lasEnfermedades = lasEnfermedades;
+		this.lasPersonas = lasPersonas;
 	}
 	
 	
-	private ArrayList<Vacuna>lasVacinas;
+	private ArrayList<Vacuna>lasVacunas;
 	private ArrayList<Vivienda>lasViviendas;
 	private ArrayList<Cita>lasCitas;
 	private ArrayList<Enfermedad>lasEnfermedades;
+	private ArrayList<Persona>lasPersonas;
 	
 	
 	
 	
-	public ArrayList<Vacuna> getLasVacinas() {
-		return lasVacinas;
+	public ArrayList<Vacuna> getLasVacunas() {
+		return lasVacunas;
 	}
-	public void setLasVacinas(ArrayList<Vacuna> lasVacinas) {
-		this.lasVacinas = lasVacinas;
+	public void setLasVacunas(ArrayList<Vacuna> lasVacunas) {
+		this.lasVacunas = lasVacunas;
 	}
 	public ArrayList<Vivienda> getLasViviendas() {
 		return lasViviendas;
@@ -45,6 +47,12 @@ public class Clinica {
 	}
 	public void setLasEnfermedades(ArrayList<Enfermedad> lasEnfermedades) {
 		this.lasEnfermedades = lasEnfermedades;
+	}
+	public ArrayList<Persona> getLasPersonas() {
+		return lasPersonas;
+	}
+	public void setLasPersonas(ArrayList<Persona> lasPersonas) {
+		this.lasPersonas = lasPersonas;
 	}
 	
 	
