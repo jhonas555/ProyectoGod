b7d837fe4f9314270cc074d3c8a3814027785c76
Jhonas <Jhonas@DESKTOP-TDAE268.Home>
2023-12-03 11:22:57 -0400

    complemento de citas

--- ClinicaJDC/src/logico/Cita.java
+++ ClinicaJDC/src/logico/Cita.java
@@ -5,6 +5,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Cita implements Serializable{
 	
@@ -16,9 +17,9 @@ public class Cita implements Serializable{
 	private Paciente paciente;
 	private Doctor doctor;
 	
-	public Cita(String id, Date fecha, Paciente paciente, Doctor doctor) {
+	public Cita(Object object, Date fecha, Paciente paciente, Doctor doctor) {
 		super();
-		this.id = id;
+		this.id = (String) object;
 		this.fecha = fecha;
 		this.paciente = paciente;
 		this.doctor = doctor;
@@ -56,6 +57,109 @@ public class Cita implements Serializable{
 	    }
 
 	}
+	
+	
+	public class AgendaCitas {
+
+	    private GestorCitas gestorCitas;
+
+	    public AgendaCitas() {
+	        this.gestorCitas = new GestorCitas();
+	    }
+
+	    public void agendaCita(Paciente paciente, Doctor doctor, Date fecha) {
+	        
+	        // crea una cita
+	        Cita cita = new Cita(id, fecha, paciente, doctor); 
+	        
+	        // lista las citas
+	        List<Cita> citas = gestorCitas.cargarCitas();
+	        
+	        // agrega citas
+	        citas.add(cita);
+
+	        // guarda las citas 
+	        gestorCitas.guardarCitas(citas);
+	        
+	    }
+	    
+	    
+	    public void cancelarCita(Cita cita) {
+	        
+	        List<Cita> citas = gestorCitas.cargarCitas();
+	        
+	        // Buscar la cita y cambiar el estado a cancelada
+	        for(Cita c : citas) {
+	            if(c.getId().equals(cita.getId())) {
+	                c.setEstado(false);
+	                break;
+	            }
+	        }
+	        
+	        gestorCitas.guardarCitas(citas);
+	        
+	    }
+	    
+	    public void actualizarCita(Cita citaNueva) {
+	    
+	        List<Cita> citas = gestorCitas.cargarCitas();
+	        
+	        // Buscar la cita vieja y actualizarla 
+	        for(int i = 0; i < citas.size(); i++) {
+	            Cita c = citas.get(i); 
+	            if(c.getId().equals(citaNueva.getId())) {
+	                citas.set(i, citaNueva); 
+	                break;
+	            }
+	        }
+
+	        gestorCitas.guardarCitas(citas);
+	        
+	    }
+
+	    
+	    private void generaId() {
+	        // pendiente generar id unico
+	    }
+
+	}
+	
+	public class ConsultaCitas {
+
+	    private GestorCitas gestorCitas;
+
+	    public ConsultaCitas() {
+	        this.gestorCitas = new GestorCitas();
+	    }
+
+	    public List<Cita> consultaCitas() {
+	        return gestorCitas.cargarCitas(); 
+	    }
+
+	    public List<Cita> consultaCitasPaciente(Paciente paciente) {
+	        List<Cita> citas = gestorCitas.cargarCitas();
+	        
+	        // Filtrar sólo citas del paciente
+	        return citas.stream()
+	                    .filter(c -> c.getPaciente().equals(paciente))
+	                    .collect(Collectors.toList());
+	    }
+	    
+	    public List<Cita> consultaCitasDoctor(Doctor doctor, Date desde, Date hasta) {
+	        List<Cita> citas = gestorCitas.cargarCitas();
+	        
+	        // Filtrar por doctor y rango de fechas
+	        return citas.stream()
+	                    .filter(c -> c.getDoctor().equals(doctor)) 
+	                    .filter(c -> c.getFecha().after(desde) && 
+	                               c.getFecha().before(hasta))
+	                    .collect(Collectors.toList()); 
+	    }
+
+	}
+	
+	
+	
 
 	public Cita() {
 	}
