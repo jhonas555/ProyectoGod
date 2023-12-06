7883be27434e6d6c54ef6133cd1781feda2bb2bb
crist <crist@192.168.56.1>
2023-12-06 15:29:39 -0400

    arreglar error campo extra

--- ClinicaJDC/src/Visual/RegAdministradores.java
+++ ClinicaJDC/src/Visual/RegAdministradores.java
@@ -18,7 +18,6 @@ public class RegAdministradores extends JPanel {
 	private JTable table;
 	private JTextField textField;
 	private JTextField textField_1;
-	private JTextField textField_2;
 	private JTextField textField_3;
 	private JTextField textField_4;
 	private JTextField textField_5;
@@ -94,11 +93,6 @@ public class RegAdministradores extends JPanel {
 		lblId.setBounds(28, 103, 135, 16);
 		add(lblId);
 		
-		JLabel lblPaciente = new JLabel("Paciente");
-		lblPaciente.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblPaciente.setBounds(28, 267, 80, 16);
-		add(lblPaciente);
-		
 		JButton button_1 = new JButton("Cita Nueva");
 		button_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -112,39 +106,34 @@ public class RegAdministradores extends JPanel {
 		textField_1.setBounds(165, 150, 330, 32);
 		add(textField_1);
 		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(165, 260, 330, 32);
-		add(textField_2);
-		
 		JLabel lblFechaNacimiento = new JLabel("Fecha Nacimiento");
 		lblFechaNacimiento.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblFechaNacimiento.setBounds(28, 322, 126, 16);
+		lblFechaNacimiento.setBounds(28, 267, 126, 16);
 		add(lblFechaNacimiento);
 		
 		textField_3 = new JTextField();
 		textField_3.setColumns(10);
-		textField_3.setBounds(165, 315, 330, 32);
+		textField_3.setBounds(165, 260, 330, 32);
 		add(textField_3);
 		
 		textField_4 = new JTextField();
 		textField_4.setColumns(10);
-		textField_4.setBounds(165, 370, 330, 32);
+		textField_4.setBounds(165, 315, 330, 32);
 		add(textField_4);
 		
 		JLabel lblTelfono = new JLabel("Tel\u00E9fono");
 		lblTelfono.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblTelfono.setBounds(28, 378, 126, 16);
+		lblTelfono.setBounds(28, 323, 126, 16);
 		add(lblTelfono);
 		
 		JLabel lblCorreoElectrnico = new JLabel("Correo Electr\u00F3nico");
 		lblCorreoElectrnico.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblCorreoElectrnico.setBounds(28, 432, 147, 16);
+		lblCorreoElectrnico.setBounds(28, 377, 147, 16);
 		add(lblCorreoElectrnico);
 		
 		textField_5 = new JTextField();
 		textField_5.setColumns(10);
-		textField_5.setBounds(165, 425, 330, 32);
+		textField_5.setBounds(165, 370, 330, 32);
 		add(textField_5);
 		
 		JLabel lblUsuario = new JLabel("Usuario");
