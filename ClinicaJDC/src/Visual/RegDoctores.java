6359489ce69c17253e3a8f5dcf478b7ac1a98829
crist <crist@192.168.56.1>
2023-12-07 00:36:47 -0400

    a

--- ClinicaJDC/src/Visual/RegDoctores.java
+++ ClinicaJDC/src/Visual/RegDoctores.java
@@ -75,7 +75,7 @@ public class RegDoctores extends JPanel {
                 agregarDoctor();
             }
         });
-        btnNewButton.setBounds(900, 851, 120, 32);
+        btnNewButton.setBounds(900, 595, 120, 32);
         add(btnNewButton);
 
         JButton btnModificar = new JButton("Guardar");
@@ -84,11 +84,11 @@ public class RegDoctores extends JPanel {
                 
             }
         });
-        btnModificar.setBounds(768, 851, 120, 32);
+        btnModificar.setBounds(768, 595, 120, 32);
         add(btnModificar);
 
         JButton btnEliminar = new JButton("Eliminar");
-        btnEliminar.setBounds(636, 851, 120, 32);
+        btnEliminar.setBounds(636, 595, 120, 32);
         add(btnEliminar);
 
         JLabel lblFecha = new JLabel("Nombre");
@@ -111,7 +111,7 @@ public class RegDoctores extends JPanel {
             public void actionPerformed(ActionEvent e) {
             }
         });
-        button_1.setBounds(504, 851, 120, 32);
+        button_1.setBounds(504, 595, 120, 32);
         add(button_1);
 
         txtNombre = new JTextField();
